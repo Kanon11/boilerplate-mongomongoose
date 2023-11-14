d8fda6419cf0f00a365604508162e100df410d65
Kanon11 <kanoncuetcse11@gmail.com>
2023-11-15 00:29:07 +0600

    fixing

--- models/Person.js
+++ models/Person.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
-        unique:true
+        required: true
     },
     age: {
         type: Number,
