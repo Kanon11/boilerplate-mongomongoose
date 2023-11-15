f2dadb0a73d0e24ae61043aa63aadc8c6ba233a4
Kanon11 <kanoncuetcse11@gmail.com>
2023-11-15 15:59:02 +0600

    fuaaaa

--- myApp.js
+++ myApp.js
@@ -84,7 +84,7 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-  Person.useFindAndModify({ name: personName }, { age: ageToSet }, (err, data) => {
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet },{new:true} ,(err, data) => {
     if (err) {
       done(err);
     }
